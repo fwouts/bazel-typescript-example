f9fa0169a6ef20078b98731381e7539edbde062a
Francois Wouts <f@zenc.io>
2018-03-21 22:17:13 +1100

    Add TSX (React) example

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
 import { sayHi } from "workspace/src/dep";
+import HelloComponent from "workspace/src/react";
 
 console.log(sayHi("World"));
+console.log("TSX is compiled too!");
+console.log(HelloComponent.toString());
---
+++ src/react.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const HelloComponent = () => {
+  return (
+    <h2>Hello component !</h2>
+  );
+}
+
+export default HelloComponent;
